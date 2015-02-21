803b7d994b74a8026f7d6ea679e674201d171baf
hakanhammarin <hakan.hammarin@gmail.com>
2015-02-21 20:32:19 +0100

    .

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 monitoring-sites
 ================
+Monitoring www.google.se and logges it to Console and Log-file
+
+run: npm install
+node app.js
--- app.js
+++ app.js
@@ -13,89 +13,89 @@ var Ping = require('./lib/ping'),
 				// },
             // timeout: 30
         // },
-        {
-            name: 'mobile.10mila.se (Internet)',
-			url: 'http://mobile.10mila.se',
-			proxy: 'http://mobile.10mila.se:80',
-			headers: {
-				'host' : 'mobile.10mila.se',
-				'Pragma' : 'no-cache',
-				'Cookie' : '',
-			
-				},
-            timeout: 30
-        },
-        {
-            name: '10mila.se',
-			url: 'http://10mila.se',
-			proxy: 'http://10mila.se:80',
-			headers: {
-				'host' : '10mila.se',
-				'Pragma' : 'no-cache',
-				'Cookie' : '',
-			
-				},
-            timeout: 30
-        },
-        {
-            name: 'dev.10milatv.se (Paywall)',
-			url: 'http://dev.10milatv.se',
-			proxy: 'http://dev.10milatv.se:80',
-			headers: {
-				'host' : 'dev.10milatv.se',
-				'Pragma' : 'no-cache',
-				'Cookie' : '',
-			
-				},
-            timeout: 30
-        },
-		{
-            name: 'OLA Startlista (Arena)',
-			url: 'http://192.168.1.23/teamtracker/startlist?searchstring=',
-			proxy: 'http://192.168.1.23:8080',
-			headers: {
-				'host' : '1.1.1.1',
-				'Pragma' : 'no-cache',
-				'Cookie' : '',
-			
-				},
-            timeout: 30
-        },
-		{
-            name: 'OLA Toplist Damer (Arena)',
-			url: 'http://192.168.1.23/teamtracker/toplist?classId=2',
-			proxy: 'http://192.168.1.23:8080',
-			headers: {
-				'host' : '1.1.1.1',
-				'Pragma' : 'no-cache',
-				'Cookie' : '',
-			
-				},
-            timeout: 30
-        },{
-            name: 'OLA Resultat Damer (Arena)',
-			url: 'http://192.168.1.23/teamtracker/resultlist?classId=2',
-			proxy: 'http://192.168.1.23:8080',
-			headers: {
-				'host' : '1.1.1.1',
-				'Pragma' : 'no-cache',
-				'Cookie' : '',
-			
-				},
-            timeout: 30
-        },
-		{
-            name: 'online (localhost)',
-			url: 'http://localhost/index.php',
-			proxy: 'http://localhost:80',
-			headers: {
-				'host' : 'localhost',
-				'Pragma' : 'no-cache',
-				'Cookie' : '',
-			
-				},
-            timeout: 30
-        },
+    //     {
+    //         name: 'mobile.10mila.se (Internet)',
+		// 	url: 'http://mobile.10mila.se',
+		// 	proxy: 'http://mobile.10mila.se:80',
+		// 	headers: {
+		// 		'host' : 'mobile.10mila.se',
+		// 		'Pragma' : 'no-cache',
+		// 		'Cookie' : '',
+		//
+		// 		},
+    //         timeout: 30
+    //     },
+    //     {
+    //         name: '10mila.se',
+		// 	url: 'http://10mila.se',
+		// 	proxy: 'http://10mila.se:80',
+		// 	headers: {
+		// 		'host' : '10mila.se',
+		// 		'Pragma' : 'no-cache',
+		// 		'Cookie' : '',
+		//
+		// 		},
+    //         timeout: 30
+    //     },
+    //     {
+    //         name: 'dev.10milatv.se (Paywall)',
+		// 	url: 'http://dev.10milatv.se',
+		// 	proxy: 'http://dev.10milatv.se:80',
+		// 	headers: {
+		// 		'host' : 'dev.10milatv.se',
+		// 		'Pragma' : 'no-cache',
+		// 		'Cookie' : '',
+		//
+		// 		},
+    //         timeout: 30
+    //     },
+		// {
+    //         name: 'OLA Startlista (Arena)',
+		// 	url: 'http://192.168.1.23/teamtracker/startlist?searchstring=',
+		// 	proxy: 'http://192.168.1.23:8080',
+		// 	headers: {
+		// 		'host' : '1.1.1.1',
+		// 		'Pragma' : 'no-cache',
+		// 		'Cookie' : '',
+		//
+		// 		},
+    //         timeout: 30
+    //     },
+		// {
+    //         name: 'OLA Toplist Damer (Arena)',
+		// 	url: 'http://192.168.1.23/teamtracker/toplist?classId=2',
+		// 	proxy: 'http://192.168.1.23:8080',
+		// 	headers: {
+		// 		'host' : '1.1.1.1',
+		// 		'Pragma' : 'no-cache',
+		// 		'Cookie' : '',
+		//
+		// 		},
+    //         timeout: 30
+    //     },{
+    //         name: 'OLA Resultat Damer (Arena)',
+		// 	url: 'http://192.168.1.23/teamtracker/resultlist?classId=2',
+		// 	proxy: 'http://192.168.1.23:8080',
+		// 	headers: {
+		// 		'host' : '1.1.1.1',
+		// 		'Pragma' : 'no-cache',
+		// 		'Cookie' : '',
+		//
+		// 		},
+    //         timeout: 30
+    //     },
+		// {
+    //         name: 'online (localhost)',
+		// 	url: 'http://localhost/index.php',
+		// 	proxy: 'http://localhost:80',
+		// 	headers: {
+		// 		'host' : 'localhost',
+		// 		'Pragma' : 'no-cache',
+		// 		'Cookie' : '',
+		//
+		// 		},
+    //         timeout: 30
+    //     },
 		{
             name: 'Google',
 			url: 'http://www.google.se',
@@ -104,7 +104,7 @@ var Ping = require('./lib/ping'),
 				'host' : 'www.google.se',
 				'Pragma' : 'no-cache',
 				'Cookie' : '',
-			
+
 				},
             timeout: 30
         },
@@ -136,6 +136,6 @@ websites.forEach(function (website) {
 server = http.createServer(function (req, res) {
     var data = "Monitoring the following websites: \n \n" + websites.join("\n");
     res.end(data);
-}); 
+});
 server.listen(port);
 console.log('Listening to port %s', port);
