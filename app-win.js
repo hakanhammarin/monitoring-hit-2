bb3bdae59d22d4f3b714bea2327e9d35da25b454
hakanhammarin <hakan.hammarin@gmail.com>
2015-02-26 09:08:01 +0100

    cleaning

--- app-win.js
+++ app-win.js
@@ -48,52 +48,7 @@ function find(key, array) {
 }
 
 
-function puts(error, stdout, stderr) {
-  sys.puts(stdout);
-    //console.log(stdout);
-    //var ret = JSON.parse(stdout);
-    //console.log(ret);
-    var res1 = stdout.split("\n");
-//    var res2 = stdout.match("/bssid.*/gi");
-//    console.log(res1);
-//  var search1 = find("B",stdout);
-var search1 = find("    BSSID",res1);
-var search2 = search1.toString().trim().split(": ");
-//var search3 = search1.toString().trim();
-//var search4 = search1.toString().trim().replace(/[:][ ]/g,'=');
-var signal1 = find("    Signal",res1);
-var signal2 = signal1.toString().trim().split(": ");
-//var signal3 = signal1.toString().trim();
-//var signal4 = signal1.toString().trim().replace(/[:][ ]/g,'=').replace(/[ ][%]/g,'');
-//"{key1:value1}{key2:value2}".replace(/^{|}$/g, '').split('}{')
-  //var res = str.toString();
-  /* console.log(search1);
-  console.log(search2);
-  console.log(search3);
-  console.log(search4);
-   */
-  bssid = search2.pop();
-  console.log("bssid="+bssid);
   
- /*  console.log(signal1);
-  console.log(signal2);
-  console.log(signal3);
-  console.log(signal4); */
-  signal = signal2.pop().replace(/[ ][%]/g,'');
-  console.log("signal="+signal);
-  // var ret = JSON.parse(stdout);
-  }
-
-
-
-
-  
-/* setInterval(function() { 
-	exec("netsh wlan show interfaces", puts);
-  }, 5000);
- */
-// *** Files
-
 
 var FFile = require('./lib/file'),
 
@@ -106,7 +61,7 @@ var FFile = require('./lib/file'),
       },
 	  { name : 'dummy.xlsx-lokalt',
         path : 'C:\\down\\monitoring\\dummy.xlsx',
-        timeout : '30'
+        timeout : '60'
       }
     ],
     monitors = [];
@@ -127,7 +82,7 @@ var Ping = require('./lib/ping'),
 				'Cookie' : '',
 
 				},
-            timeout: 30
+            timeout: 60
         },
         {
             name: '1MBAtSunet',
@@ -137,7 +92,7 @@ var Ping = require('./lib/ping'),
 				'host' : 'ftp.sunet.se',
 				'Pragma' : 'no-cache',
 				},
-			timeout: 30
+			timeout: 600
         },
         {
             name: 'Cacti',
@@ -147,7 +102,7 @@ var Ping = require('./lib/ping'),
 				'host' : 'mgmtcacti.intern.hoglandet.se',
 				'Pragma' : 'no-cache',
 				},
-			timeout: 15
+			timeout: 60
         }
     ],
     http = require('http'),
